af282873c1614afdbf6328c1abd175ae52d1cb01
Vikas Bhalla <77003706+vikasbhalla05@users.noreply.github.com>
2021-07-13 13:47:07 +0530

    Finding max and min number in three numbers

---
+++ minMaxThreeNumbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,n3;
+    cout<<"Enter three numbers:"<<endl;
+    cin>>n1>>n2>>n3;
+    
+    if(n1>n2){
+        if(n1>n3){
+            cout<<n1<<" is the greatest number"<<endl;
+        }
+        else{
+            cout<<n1<<" is the greatest number"<<endl;
+        }
+    }
+    else{
+        if(n2>n3){
+            cout<<n2<<" is the greatest number"<<endl;
+        }
+        else{
+            cout<<n3<<" is the greatest number"<<endl;
+        }
+    }
+    return 0;
+}
